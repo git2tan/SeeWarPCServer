0ce8d5e350641d39c7245273b8b8f2efac4fef2c
git2tan <git2tan@mail.ru>
2017-06-20 19:50:44 +0700

    исправлен коммент

--- src/Connector.java
+++ src/Connector.java
@@ -19,12 +19,8 @@ public class Connector implements Runnable {
             while(true){
                 Socket tmpSocket = null;
                 tmpSocket = serverSocket.accept();
-                if(tmpSocket!=null) {                                         //при подключении создает нового анонимного серверного геймера
-//                    ServerGamer gamer = new ServerGamer(tmp, myServer); //этому серверному геймеру сразу задается сервер в виде порта и ссылки на сам сервер
-//                    myServer.addAnonimGamer(gamer);                     //вызывается запрос к серверу на добавление игрока
-//
-//                    gamer.sendMessage(new Message(101,"Anonim"+myServer.incrementIndexOfAnonim()));
-//                    myServer.sendMessageToAll(new Message(200,"SERVER!","Подключился новый клиент!"));
+                if(tmpSocket!=null) {
+                    //при подключении создает нового анонимного серверного геймер
                     System.out.println("Подключен новый пользователь from " + tmpSocket.getInetAddress().toString());
                     ServerGamer gamer = new ServerGamer(tmpSocket, processor);
                     System.out.println("Жду нового пользователя");
